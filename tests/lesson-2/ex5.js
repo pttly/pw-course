b292c5fd9302b7d6aea33bc79bd3160dc0a1a457
PW - Ly <thaoly.phan197@gmail.com>
2025-06-17 21:56:25 +0700

    chore: update ex5 ex8 lesson-2

--- tests/lesson-2/ex5.js
+++ tests/lesson-2/ex5.js
@@ -1,6 +1,6 @@
 //syntax loop: for(<khởi tạo>; <điều kiện dừng>; <điều kiện tăng>) {code}
 
-for (let i = 1; i <= 3; i++) {
+for (let i = 1; i <= 100; i++) {
     if (i % 2 === 0) {
         console.log("Số " + i + " là số chẵn");
     } else {
